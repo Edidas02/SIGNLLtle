4e087f662472437f7d52c378046b3f4929fc944c
Edward Xiong <53875786+Edidas02@users.noreply.github.com>
2022-04-18 18:41:11 -0500

    discord api

--- semantle_client.py
+++ semantle_client.py
@@ -9,9 +9,6 @@ game_sessions = {}
 def fetch_session_id(message):
     return str(message.guild.id) + str(message.channel.id)
 
-
-
-
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 PUBLIC_KEY = os.getenv('PUBLIC_KEY')
 client = discord.Client()
@@ -24,8 +21,18 @@ async def on_ready():
 @client.event
 async def on_message(message):
     # ignore bot's command
-    if message.author == client.user:
+    if message.author == client.user or message.content[0] != '!':
         return
+    command = message.content[1::].lower()
+    print(command[0])
+    if len(command) == 1 and command[0] == "p":
+        await message.channel.send("Your game has started")       #initiate a game
+    if len(command) >= 2 and command[0] == "g":
+        await message.channel.send("Your guess is: " + message.content[3::].lower())            #quit game
+    if len(command) == 1 and command[0] == "q":
+        await message.channel.send("Game has ended!")              #make a guess
+
+
     print(message.content)
     print(fetch_session_id(message))
     await message.channel.send("Hello!")
