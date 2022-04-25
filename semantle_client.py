ffe62a53cf356a3ab29ca9b39d02a1580fdad6a7
Apoorva Aditya <apoorvaufo3@gmail.com>
2022-04-25 18:52:04 -0500

    added game functionality to bot

--- semantle_client.py
+++ semantle_client.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import discord
+from game import Game
 
 load_dotenv()
 
@@ -23,20 +24,31 @@ async def on_message(message):
     # ignore bot's command
     if message.author == client.user or message.content[0] != '!':
         return
+    
+    session_id = fetch_session_id(message)
+
     command = message.content[1::].lower()
     print(command[0])
     if len(command) == 1 and command[0] == "p":
+        game_sessions[session_id] = Game(session_id)
         await message.channel.send("Your game has started")       #initiate a game
     if len(command) >= 2 and command[0] == "g":
-        await message.channel.send("Your guess is: " + message.content[3::].lower())            #quit game
+        guess = message.content[3::].lower()
+        await message.channel.send("Your guess is: " + guess)            #quit game
+        if session_id in game_sessions.keys():
+            similarity = game_sessions[session_id].make_guess(guess)
+            await message.channel.send("The similarity is: " + similarity)
+        else:
+            await message.channel.send("Start a game before guessing")
     if len(command) == 1 and command[0] == "q":
+        game_sessions.pop(session_id)
         await message.channel.send("Game has ended!")              #make a guess
 
 
     print(message.content)
-    print(fetch_session_id(message))
+    print(session_id)
     await message.channel.send("Hello!")
 
-client.run(os.getenv('BOT_TOKEN'))
+client.run(BOT_TOKEN)
 
 
